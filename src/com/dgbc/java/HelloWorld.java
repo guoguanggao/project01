f236c1569f9c9d2a1bc23540f1557a499c65a3b2
zhangsan <zhangsan@itcast.cn>
2019-07-28 23:42:46 +0800

    Initial commit

---
+++ src/com/dgbc/java/HelloWorld.java
@@ -0,0 +1,68 @@
+package com.dgbc.java;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+    private static final int I =1;
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    public static void main(String[] args) {
+        system.out.println("helloworld!!!");
+        system.out.println("helloworld!!!");
+        system.out.println("helloworld!!!");
+
+        system.out.println("helloworld!!!");
+        ARRAYLIST list = new arraylist();
+        System.out.println("args = [" + args + "]");
+        System.out.println("HelloWorld.main");
+        System.out.println("list = " + list);
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("123");
+        list1.add("145");
+        list1.add("157");
+
+        for (String s : list1) {
+            System.out.println("s = " + s);
+        }
+        for (int i = 0; i < list1.size(); i++) {
+
+        }
+        for (int i = list1.size() - 1; i >= 0; i--) {
+
+        }
+        if (list1 == null) {
+
+        }
+        if (args == null) {
+
+        }
+        if (list1 != null) {
+
+        }
+
+        if (list != null) {
+
+        }
+        if (list == null) {
+
+        }
+
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    public void method(){
+//        arra
+//String/**/
+        int i = 1;
+    }
+}
---
+++ src/com/dgbc/java/Student.java
@@ -0,0 +1,9 @@
+package com.dgbc.java;
+
+/**
+ * @author 郭光高
+ * @create 2019-07-28 16:26
+ */
+public class Student {
+
+}
